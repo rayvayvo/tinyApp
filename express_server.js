7c5853c916e0af1087298d730d751bb84ce91464
Raven Ackerman <rayvayvo@gmail.com>
2017-03-15 19:26:46 +0000

    added a git ignore file that I should have done earlier. also added an unworking login form/button.

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
-
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -59,7 +61,7 @@ app.get("/urls/:id", (req, res) => {
   let short_url = req.params.id
   let long_url = urlDatabase[short_url]
   let templateVars = { shortURL: short_url, longURL: long_url };
-  console.log(templateVars);
+  // console.log(templateVars);
   res.render("urls_show", {templateVars});
 });
 
