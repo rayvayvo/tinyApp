c8c0a00e0d1a0d46e8105fc5e1f0dc7721398667
Raven Ackerman <rayvayvo@gmail.com>
2017-03-17 17:58:47 +0000

    register button now properly creates a cookie

--- express_server.js
+++ express_server.js
@@ -12,7 +12,6 @@ app.set("view engine", "ejs");
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-  //add new short and long url keys
 };
 
 const users = {
@@ -33,7 +32,7 @@ app.get("/urls", function(req, res) {
   let short_url = req.params.id
   let long_url = urlDatabase[short_url]
   let templateVars = { shortURL: short_url, longURL: long_url,
-  username: req.cookies["userName"] };
+  user: req.cookies["userName"] };
   res.render("urls_index", { urlDatabase, templateVars });
 });
 
@@ -41,24 +40,36 @@ app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
+// app.get("/urls/new", (req, res) => {
+//   res.render("urls_new");
+// });
 
 app.get("/register", (req, res) => {
   res.render("urls_register");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/error", (req, res) => {
+  res.render("error");
+});
+
+// app.post("/urls", (req, res) => {
+//   console.log(req.body);
+//   res.send(generateRandomString);         // Respond with 'Ok' (we will replace this)
+// });
+
+app.post("/create", (req, res) => {
+  let shortURL = generateRandomString();         // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortURL] = req.body.addURL;
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
 if (urlDatabase[req.params.id]) {
   delete urlDatabase[req.params.id]
-} else {
-// print errorr saying id not found
 }
 res.redirect("/urls");
 });
@@ -80,7 +91,7 @@ app.get("/urls/:id", (req, res) => {
   let short_url = req.params.id
   let long_url = urlDatabase[short_url]
   let templateVars = { shortURL: short_url, longURL: long_url,
-  username: req.cookies["userName"] };
+  user: req.cookies["user_id"] };
   // console.log(templateVars);
   res.render("urls_show", {templateVars});
 });
@@ -93,71 +104,85 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[longURL]);
 });
 
-app.post("/urls/login", (req, res) => {
-  let userName = req.body.userName;
-  res.cookie('userName', userName);
-  console.log(req.body);
+app.post("/login", (req, res) => {
+// console.log("req.email value here: " + req.body.email);
+// console.log("password: " + req.body.password)
+  for (email in users) {
+    if (req.body.email == users.email) {
+      var user = users.id;
+      res.cookie('user_id', user);
+    } else {
+      res.redirect("/error");
+    }
+  }
   res.redirect("/urls");
 });
+//cookie needs to read login email, find it in the database, match it to an id and set that id value as user_id
+
 
 app.post("/urls/logout", (req, res) => {
-  res.clearCookie('userName');
+  res.clearCookie('user_id');
   res.redirect("/urls");
 });
 
 
 
-// app.post("/register", (req, res) => {
-//   // let userID = (Math.random() + 1).toString(36).slice(0, 8);
-//   // let userInfo = {
-//   //   userID: {
-//   //       id: userID,
-//   //       email: req.body.email,
-//   //       password: req.body.password
-//   //     }
-//   // }
-// // users.push(userID);
-// // console.log(users);
-//   res.redirect("/urls");
-// });
+app.post("/register", (req, res) => {
+
+  if ( req.body.email.length > 0 && req.body.password.length > 0) {
 
 
+      // for (users.email in users) {
 
-// const users = {
-//   "userRandomID": {
-//     id: "userRandomID",
-//     email: "user@example.com",
-//     password: "purple-monkey-dinosaur"
-//   },
-//  "user2RandomID": {
-//     id: "user2RandomID",
-//     email: "user2@example.com",
-//     password: "dishwasher-funk"
-//   }
-// }
+        // if (users.email == req.body.email) {
+        //   need to fix email checking error. doesnt correctly check email value
+        //   thinking I need to loop through users, compare email values, if they're the same, return error "email already in use"
+        //   res.redirect("/error");
 
+        // } else {
+
+          let userID = (Math.random() + 1).toString(36).slice(2, 8);
+          let userInfo = {
+            id: userID,
+            email: req.body.email,
+            password: req.body.password
+          }
+      users[userID] = userInfo;
+
+      console.log("users email right hurr" + users[userID].email);
+      console.log("users pass right hurr" + users[userID].password);
+      console.log("user ID here: " + users[userID].id);
+
+      res.cookie('User_id', userInfo)
+      res.redirect("/urls");
+
+      //   }
+      // }
+  } else {
+
+      res.redirect("/error");
+
+
+    }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 
-//make a function that generates 6 random characters and sets it as the short URL
-//characters should be alpha-numeric, capitals are fine
+//function that generates 6 random characters and sets it as the short URL
 
 function generateRandomString() {
 var changeLetter = ""
 
-    changeLetter = (Math.random() + 1).toString(36).slice(0, 8);
+    changeLetter = (Math.random() + 1).toString(36).slice(2, 8);
 return changeLetter;
 }
 
-
-// modify register to take in the email and password and pass it along as new object values into the "user" database
-// along with setting new user email and password keys, also generate a random string for a user ID and set that as the user ID key
-// once those values are set, create a cookie with all three keys.
 // have every page check if there is a cookie with all 3 keys present, if not, have a link to log in on the page
-//
+//make error for invalid password entry
+//make error for existing email
 
 
 
